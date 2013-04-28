f9afedc793cd1d814b4081f2a3f1389804b7c8d6
dludaya09 <dludaya.cs@gmail.com>
2013-04-28 19:21:38 -0300

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+</head>
+<body bgcolor="pink">
+<font size="10" face="brush script mt""> Jingo<br>
+<p align="left"><font size="4" face="century gothic">*Share best places with people*</p>
+<form>
+<p align="right"><font size="5" face="tempus sans itc">Sign In</p>
+<p1 align="right"><font size="4" face="century gothic"></p1>
+<div style="float: right; text-align: right; vertical-align:middle;">
+Userid <input type="text" name="userid"><br>
+Password <input type="text" name="password"><br>
+<button style="width:85; height:35">Log In</button><br><br>
+<p2 align="right"><font size="5" face="tempus sans itc">Sign Up.. Its Always Free</p2><br><br>
+<p3 align="right"><font size="4" face="century gothic"></p3>
+<div style="float: right; text-align: right; vertical-align:middle;">
+Userid <input type="text" name="userid"><br>
+Password <input type="text" name="password"><br>
+FirstName <input type="text" name="firstname"><br>
+MiddleName <input type="text" name="middlename"><br>
+LastName <input type="text" name="lastname"><br>
+Email <input type="text" name="mailid"><br>
+<input type="radio" name="Gender" value="male">Male
+<input type="radio" name="Gender" value="female">Female<br>
+<button style="width:85; height:35">Sign Up</button><br><br>
+</div>
+</form>
+<div align="left"> </div>
+</font>
+</body>
+</html>
