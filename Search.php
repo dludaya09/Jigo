ee7aea6c9d1a400ba0fab75d8e858527a6f44ddd
dludaya09 <dludaya.cs@gmail.com>
2013-05-01 14:20:23 -0300

    Create Search.php

---
+++ Search.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+  <form action="login.php" method="post">
+<p align="left"><font size="10" face="brush script mt"></p>
+<table align="left">
+<tr>
+<td bgcolor="#F778A1" width="2000" height="100"><font size="10"><b> Jingo</b><br><font size="4" face="century gothic">*Share best places with people*</font></font></td>
+</tr>
+<tr>
+<td>
+<font size="6" face="tempus sans itc">
+<a href="#search" data-toggle="tab" id="search">Search a Location</a><br>
+<a href="#editprofile" data-toggle="tab" id="editprofile" onClick="Edit()">Edit Profile</a>
+</td>
+</tr>
+</table>
+<script type="text/javascript">
+function Edit(){
+	window.open("http://localhost/Jingo/Jingo/EditProfile.php");
+}
+</script>
+</font>
+</form>
+</body>
+</html>
