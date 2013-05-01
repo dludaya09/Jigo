626dc31bba2741e5c0415bbb55d663b481622b02
dludaya09 <dludaya.cs@gmail.com>
2013-05-01 14:18:47 -0300

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<p align="left"><font size="10" face="brush script mt"></p>
+<table align="left">
+<tr>
+<td bgcolor="#F778A1" width="2000" height="100"><font size="10"><b> Jingo</b><br><font size="4" face="century gothic">*Share best places with people*</font></font></td>
+</tr>
+<tr>
+<td>
+<br><br><br><br>
+<button style="width:1350; height:35" value="Log In" onClick="Login()"><font size="4">Log In</button><br><br>
+</font>
+</td>
+</tr>
+<tr>
+<td><p align="center"><font size="6" face="tempus sans itc"><br>OR
+</font>
+<tr>
+<td>
+<br><br><br>
+<button style="width:1350; height:35" value="Sign Up" onClick="Signup()"><font size="4">Sign Up</button><br><br>
+</font>
+</td>
+</tr>
+</table>
+<script type="text/javascript">
+function Login(){
+window.open("http://localhost/Jingo/Jingo/Login.php");
+}
+function Signup(){
+window.open("http://localhost/jingo/jingo/signup.php");
+}
+</script>
+</body>
+</html>
